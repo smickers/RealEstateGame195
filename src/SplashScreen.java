1ad0d93536f836e95a14cf76d805867b263e640c
Cameron Auser <auser5707@saskpolytech.ca>
2016-05-09 10:51:48 -0600

    Working on the intial GUI classes, and implementing save/load game features within them.
    -- Cam
    -- Travis

---
+++ src/SplashScreen.java
@@ -0,0 +1,103 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+
+public class SplashScreen
+{
+    public static final String SAVE_FILE_NAME = "Pokemon.opoly";
+    private static final int BUTTON_WIDTH = 200;
+    private static final int BUTTON_HEIGHT = 40;
+    private static final Font BUTTON_FONT = new Font("Comic Sans MS", 26.0);
+    
+    public SplashScreen(Stage toUpdate)
+    {
+        buildSplashScreen();
+    }
+    
+    public void buildSplashScreen()
+    {
+        VBox main = new VBox();
+        main.setAlignment(Pos.CENTER);
+
+        // Set up ImageView for logo
+        ImageView logo = new ImageView(new Image("img/logo.png"));
+
+        Button btnNewGame = new Button("New Game");
+        Button btnLoadGame = new Button("Load");
+        Button btnExit = new Button("Exit");
+
+        btnNewGame.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        btnLoadGame.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        btnExit.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+
+        btnNewGame.setFont(BUTTON_FONT);
+        btnLoadGame.setFont(BUTTON_FONT);
+        btnExit.setFont(BUTTON_FONT);
+
+        main.setSpacing(10.0);
+
+        main.getChildren().addAll(logo, btnNewGame, btnLoadGame, btnExit);
+
+        Scene scene = new Scene(main);
+//        stage.setScene(scene);
+//        stage.show();
+
+        btnNewGame.setOnAction(new EventHandler<ActionEvent>()
+        {
+            public void handle( ActionEvent event )
+            {
+                // TODO Start a new game
+            }
+
+        });
+
+        btnLoadGame.setOnAction(new EventHandler<ActionEvent>()
+        {
+
+            @Override
+            public void handle( ActionEvent event )
+            {
+                // Attempt to load an existing game file
+                try
+                {
+                    Object game = LoadFile.load("Pokemon.opoly");
+                }
+                catch ( Exception e )
+                {
+                    // TODO Auto-generated catch block
+                    btnLoadGame.setDisable(true);
+                    Alert alert = new Alert(AlertType.ERROR, "No save file was found!");
+                    alert.show();
+                }
+            }
+
+        });
+
+        btnExit.setOnAction(new EventHandler<ActionEvent>()
+        {
+            public void handle( ActionEvent event )
+            {
+                try
+                {
+                    System.out.println("Stopping");
+                    // TODO Pass a stage into this class' constructor
+//                    stage.close();
+                }
+                catch ( Exception e )
+                {
+                    System.out.println("Exception caught!");
+                }
+            }
+        });
+    }
+}
