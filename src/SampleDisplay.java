a1a0b6355c92d8ee7653e9ed8fa482e7eca5bc1a
Nathan MacNeil <macneil4376@saskpolytech.ca>
2016-05-11 10:22:43 -0600

    Switching seats.
    --Cam
    --Nathan

---
+++ src/SampleDisplay.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class SampleDisplay extends Application
+{
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+    
+
+public void start(Stage stage)
+{
+    
+}
+}
