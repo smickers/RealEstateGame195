302fb17d3372def0d14ee74f31dfd6d5ec7815b8
Paz Aloni <aloni3838@saskpolytech.ca>
2016-05-16 08:15:36 -0600

    Story ready for demo.	--Paz	--Stephanie

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
@@ -13,6 +14,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -41,8 +43,8 @@ public class GameBoard extends GridPane
 		this.add(new ImageView(BACKGROUND_IMAGE), 0, 0, 40, 40);
 		
 		//keeps track inside the loop of rows and columns to add in the grid pane
-		int col = 0;
-		int row = 0;
+		int col = 10;
+		int row = 10;
 		
 		//generate all the square "place-holders" for the grid pane
 		for(int i=0 ; i < GameMain.NUM_OF_TILES ; i++)
@@ -62,6 +64,16 @@ public class GameBoard extends GridPane
 				//vertical tile
 				tilesOverlay[i] = new Rectangle(wideBorder, narrowBorder, CLEAR);
 			}
+			
+			
+			//####################
+			//## For DEMO Only! ##
+			//####################
+			//for acceptance testing - display borders
+			tilesOverlay[i].setStroke(Color.RED);
+			this.add(new Text("\t"+i), col , row);
+			
+			//### END OF DEMO ###
 			
 			//add the placeholder to the grid, based on the order of the grids
 			//left column
@@ -89,8 +101,9 @@ public class GameBoard extends GridPane
 				row++;
 			}
 			
-			//for acceptance testing - display borders
-			tilesOverlay[i].setStroke(Color.RED);
+			
+			
+			
 		}
 		
 		
