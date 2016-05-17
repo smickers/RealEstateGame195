c73d29f7158b24ecfd6cea590c2aa84c8ac79d33
Hilary <fehr73021@saskpolytech.ca>
2016-05-17 08:50:36 -0600

    Working on dice roll view --Hilary --Nathan

--- src/DiceView.java
+++ src/DiceView.java
@@ -2,8 +2,15 @@ import java.util.Vector;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -15,21 +22,86 @@ public class DiceView extends Application {
     private Thread dieOne, dieTwo;
     private static ImageView dice1, dice2;
 
+    //For testing only, delete later
+    public boolean oneDiceRoll;
     
     public DiceView()
     {
+    	
+    	
         dice1 = new ImageView();
         dice2 = new ImageView();
         obtainImages();
         
-        dice1.setImage(faces.get((int)(Math.random() * 6)));
-        dice2.setImage(faces.get((int)(Math.random() * 6)));
+        dice1.setImage(faces.get((int)(Math.random() * 5)));
+        dice2.setImage(faces.get((int)(Math.random() * 5)));
     }
     
+    public DiceView(boolean oneDiceRoll)
+    {
+    	//TODO This value will be passed in
+    			//replace with this.twoDiceRoll = twoDiceRoll
+    	    	this.oneDiceRoll = false; 
+  	}
+    
+    /**
+     * will be changed to pass pane to calling method
+     */
 	@Override
-	public void start(Stage arg0) throws Exception 
+	public void start(Stage stage) throws Exception 
 	{
+		HBox dieView = new HBox();
+		VBox base = new VBox();
+		Button roll = new Button("Roll Dice");
 		
+		//specifies battle or move roll
+		//1 die for battle, 2 for move
+		if( oneDiceRoll == true)
+		{
+			dieView.getChildren().addAll(dice1);
+
+			
+			roll.setOnAction(new EventHandler<ActionEvent>()
+					{
+
+				@Override
+				public void handle(ActionEvent arg0) {
+					//dieOne = new Thread();
+					Spinners test = new Spinners(dice1, 3);
+					test.start();
+					//dieOne.start();
+
+				}
+
+					});
+			base.getChildren().addAll(dieView, roll);
+		}
+		else
+		{
+			dieView.getChildren().addAll(dice1, dice2);
+
+			
+			roll.setOnAction(new EventHandler<ActionEvent>()
+					{
+
+				@Override
+				public void handle(ActionEvent arg0) {
+
+					Spinners test = new Spinners(dice1, 3);
+					test.start();
+					Spinners test2 = new Spinners(dice2, 3);
+					test2.start();
+
+
+				}
+
+					});
+			base.getChildren().addAll(dieView, roll);
+		}
+		
+		Scene scene = new Scene(base);
+		stage.setScene(scene);
+		stage.show();
 		
 	}
 	
@@ -66,17 +138,19 @@ public class DiceView extends Application {
 	                            @Override
 	                            public void run()
 	                            {
-	                                img.setImage(faces.get((int)(Math.random() * 6)));
+	                                img.setImage(faces.get((int)(Math.random() * 5)));
 	                            }
 	                        });
 	                    try
 	                    {
-	                        Thread.sleep(20);
+	                    	
+	                        Thread.sleep(130);
 	                    }
 	                    catch (InterruptedException e)
 	                    {
 	                        currentThread().interrupt();
-	                    }  
+	                    } 
+	                    
 	                }
 	            }
 	        }
@@ -121,7 +195,7 @@ public class DiceView extends Application {
 	    private void obtainImages()
 	    {
 	        faces = new Vector<Image>(MAX_SLOT_PICS);
-	        for(int i = 0; i < MAX_SLOT_PICS; i++)
+	        for(int i = 1; i < MAX_SLOT_PICS; i++)
 	        {
 	            faces.add(new Image("./img/img" + i + ".png"));
 	        }
