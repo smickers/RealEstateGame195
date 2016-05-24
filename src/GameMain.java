1e7e138d2a79a8aa85d88f418288fdfee4ef84aa
Stephanie McCallum <mccallum0513@saskpolytech.ca>
2016-05-24 09:21:40 -0600

    Merged to master! --K&S

--- src/GameMain.java
+++ src/GameMain.java
@@ -3,15 +3,13 @@ import java.util.List;
 
 /*
  *  [File header includes information about the file being submitted.]
- *  Date submitted:
- *  Assignment number:
  *  Course name:  COSC 190
  *  Instructors: Sharon McDonald, Nelson Wong
  * 
  */
 
 /**
- * @author cst104
+ * @author Stephanie M, Kyler Nyhagen
  *
  */
 public class GameMain
@@ -28,14 +26,13 @@ public class GameMain
     private int currentPlayer;
     private int wPToWin;
     public Player player1, player2, player3, player4;
+    public boolean debug = false;
 
     public static final int NUM_OF_TILES = 40;
     
     public GameMain(ArrayList<String> playerNames)
     {
     	players = new ArrayList<Player>();
-        
-    	
     	/*
     	 * There was an if statement block here that set the
     	 * win points to win based on the number of players in the
@@ -43,29 +40,28 @@ public class GameMain
     	 * 6 (minus) the amount of players. --Kyler N
     	 */
     	wPToWin = 6 - playerNames.size();
-        System.out.println("Win points needed = " + wPToWin);
-        
+    	if(debug)
+    	{
+    	    System.out.println("Win points needed = " + wPToWin);
+    	}
+
         for(String p : playerNames)
         {
         	//turn order is 0, 1, 2, 3. NOT 1,2,3,4 --Kyler N
         	players.add(new Player(p, playerNames.indexOf(p)));
         }
         
-        System.out.println("outputting players:");
-        for(Player p : players)
+        if(debug)
         {
-        	System.out.println(p.name + " With turn position of " + p.turnOrder);
+            System.out.println("outputting players:");
+            for(Player p : players)
+            {
+                System.out.println(p.name + " With turn position of " + 
+            p.turnOrder);
+            }
         }
-        
-        
-       
     }
-    
-//    private void initializeBoard()
-//    {
-//        
-//    }
-//    
+
     /**
      * Will create every base evolution of every pokemon, no need to make the 
      * 2nd and third evolutions
